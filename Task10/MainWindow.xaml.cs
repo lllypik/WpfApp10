624168e7487ac3b5d6e7d7edb032fbbd56a8aec9
lllypik <blesphoton@gmail.com>
2022-08-16 22:32:44 +0300

    Добавьте файлы проекта.

---
+++ Task10/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Shapes;
+
+namespace Task10
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window /*, INotifyPropertyChanged*/
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //    ImagePath = "Data/IMG1.jpg";
+            //}
+
+            //public event PropertyChangedEventHandler PropertyChanged;
+            //public void OnPropertyChanged([CallerMemberName] string prop = "")
+            //{
+            //    if (PropertyChanged != null)
+            //        PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            //}
+
+
+            //private string imagePath;
+
+            //public string ImagePath
+            //{
+            //    get { return imagePath; }
+            //    set
+            //    {
+            //        imagePath = value;
+            //        OnPropertyChanged("ImagePath");
+            //    }
+            //}
+
+            //private void GetIMG1Executed(object sender, ExecutedRoutedEventArgs e)
+            //{
+            //    ImagePath = "Data/IMG1.jpg";
+            //}
+            //private void GetIMG2Executed(object sender, ExecutedRoutedEventArgs e)
+            //{
+            //    ImagePath = "Data/IMG2.jpg";
+            //}
+            //private void GetIMG3Executed(object sender, ExecutedRoutedEventArgs e)
+            //{
+            //    ImagePath = "Data/IMG3.jpg";
+            //}
+            //private void GetIMG4Executed(object sender, ExecutedRoutedEventArgs e)
+            //{
+            //    ImagePath = "Data/IMG4.jpg";
+            //}
+            //private void GetIMG5Executed(object sender, ExecutedRoutedEventArgs e)
+            //{
+            //    ImagePath = "Data/IMG5.jpg";
+            //}
+
+        }
+    }
+}
---
+++ Task10/MyCommands.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Task10
+{
+    class MyCommands
+    {
+        public static RoutedCommand GetIMG1 { get; set; }
+        public static RoutedCommand GetIMG2 { get; set; }
+        public static RoutedCommand GetIMG3 { get; set; }
+        public static RoutedCommand GetIMG4 { get; set; }
+        public static RoutedCommand GetIMG5 { get; set; }
+
+        static MyCommands()
+        {
+            GetIMG1 = new RoutedCommand("GetIMG1", typeof(MyCommands));
+            GetIMG2 = new RoutedCommand("GetIMG2", typeof(MyCommands));
+            GetIMG3 = new RoutedCommand("GetIMG3", typeof(MyCommands));
+            GetIMG4 = new RoutedCommand("GetIMG4", typeof(MyCommands));
+            GetIMG5 = new RoutedCommand("GetIMG5", typeof(MyCommands));
+        }
+    }
+}
